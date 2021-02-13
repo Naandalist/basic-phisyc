63e1193b824b58853a86182f15e5e746db64b6cc
Naandalist <listiananda.apriliawan@gmail.com>
2021-02-13 17:18:42 +0700

    Add Giffy Message

--- lib/src/helpers/error_messages.dart
+++ lib/src/helpers/error_messages.dart
@@ -1,8 +1,10 @@
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:giffy_dialog/giffy_dialog.dart';
 
 class ErrorMessage {
-  String message;
+  String message, title, desc;
+
   static void flash(message) {
     Fluttertoast.showToast(
         msg: message,
@@ -11,5 +13,33 @@ class ErrorMessage {
         backgroundColor: Color(0xffeb3b5a),
         textColor: Colors.white,
         fontSize: 16.0);
+  }
+
+  static void giffy(title, desc, context) {
+    showDialog(
+        context: context,
+        builder: (_) => AssetGiffyDialog(
+              // key: keys[5],
+              image: Image.asset(
+                'assets/animate/animation_500_error.gif',
+                fit: BoxFit.cover,
+              ),
+              title: Text(
+                title,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.w600),
+              ),
+              entryAnimation: EntryAnimation.BOTTOM_RIGHT,
+              description: Text(
+                desc,
+                textAlign: TextAlign.center,
+                style: TextStyle(),
+              ),
+              onlyOkButton: true,
+              buttonOkColor: Color(0xff6D214F),
+              onOkButtonPressed: () {
+                Navigator.of(context).pop();
+              },
+            ));
   }
 }
