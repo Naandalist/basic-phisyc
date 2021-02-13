ab7aff7f014d495bd84a0ca2adb1444d62aaebdd
Naandalist <listiananda.apriliawan@gmail.com>
2021-02-13 16:34:45 +0700

    Keyboard Dismiss From Loss Focus

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // final textTheme = Theme.of(context).textTheme;
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // theme: ThemeData(primarySwatch: Colors.green, fontFamily: 'Nunito'),
-      home: LoginScreen(),
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+
+        if (!currentFocus.hasPrimaryFocus &&
+            currentFocus.focusedChild != null) {
+          currentFocus.focusedChild.unfocus();
+        }
+      },
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        // theme: ThemeData(primarySwatch: Colors.green, fontFamily: 'Nunito'),
+        home: LoginScreen(),
+      ),
     );
   }
 }
