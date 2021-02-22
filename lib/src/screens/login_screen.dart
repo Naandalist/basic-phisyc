ac2d2ee07cbcaca1069defd5ccaa066d1f9d4720
Naandalist <listiananda.apriliawan@gmail.com>
2021-02-23 06:49:56 +0700

    Fix Login Validation

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,8 +1,8 @@
+import 'package:basicPhisyc/src/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:basicPhisyc/src/helpers/bezier_container.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:giffy_dialog/giffy_dialog.dart';
 import 'package:basicPhisyc/src/helpers/error_messages.dart';
+import '../helpers/credentials.dart' as credentials;
 
 class LoginScreen extends StatefulWidget {
   LoginScreen({Key key, this.title}) : super(key: key);
@@ -151,7 +151,8 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
-    // print(height * 0.05);
+
+    print(credentials.registeredUsername);
     return Scaffold(
         body: Container(
       // height: height,
@@ -193,50 +194,14 @@ class _LoginScreenState extends State<LoginScreen> {
       ErrorMessage.flash("Password field can't empty");
     } else if (_passwordController.text.length < 6) {
       ErrorMessage.flash("Password field can't less than 6 digits");
+    } else if (_usernameController.text == credentials.registeredUsername ||
+        _passwordController.text == credentials.registeredPassword) {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+        return HomeScreen();
+      }));
     } else {
-      // print('Ready to login');
       ErrorMessage.giffy(
-          'Login Failed', 'Invalid Username or Password.', context);
-      // _errorGiffyMessage('Login Failed', 'Invalid Username or Password.')
-
+          'Login Failed', 'Invalid sUsername or Password.', context);
     }
   }
-
-  // void _errorFlashMessage(message) {
-  //   Fluttertoast.showToast(
-  //       msg: message,
-  //       toastLength: Toast.LENGTH_SHORT,
-  //       gravity: ToastGravity.BOTTOM,
-  //       backgroundColor: Color(0xffeb3b5a),
-  //       textColor: Colors.white,
-  //       fontSize: 16.0);
-  // }
-
-  // void _errorGiffyMessage(title, desc) {
-  //   showDialog(
-  //       context: context,
-  //       builder: (_) => AssetGiffyDialog(
-  //             // key: keys[5],
-  //             image: Image.asset(
-  //               'assets/animate/animation_500_error.gif',
-  //               fit: BoxFit.cover,
-  //             ),
-  //             title: Text(
-  //               title,
-  //               textAlign: TextAlign.center,
-  //               style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.w600),
-  //             ),
-  //             entryAnimation: EntryAnimation.BOTTOM_RIGHT,
-  //             description: Text(
-  //               desc,
-  //               textAlign: TextAlign.center,
-  //               style: TextStyle(),
-  //             ),
-  //             onlyOkButton: true,
-  //             buttonOkColor: Color(0xff6D214F),
-  //             onOkButtonPressed: () {
-  //               Navigator.of(context).pop();
-  //             },
-  //           ));
-  // }
 }
